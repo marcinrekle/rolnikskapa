e336fb2672558d5145d09518a6dda377377d3818
marcinrekle <marcinrekle@gmail.com>
2021-06-02 09:08:47 +0200

    Update polacz.php

--- polacz.php
+++ polacz.php
@@ -1,5 +1,5 @@
 <?php
-mysql_connect("mysql.cba.pl","admin_marcin","haslorolnik") or die(mysql_error()."Nie mozna polaczyc sie z baza danych. Prosze chwile odczekac i sprobowac ponownie.");
+mysql_connect("host","login","password") or die(mysql_error()."Nie mozna polaczyc sie z baza danych. Prosze chwile odczekac i sprobowac ponownie.");
 mysql_select_db("rolnikskapa_cba_pl") or die(mysql_error()."Nie mozna wybrac bazy danych.");
 mysql_query("SET CHARSET utf8");
 mysql_query("SET NAMES 'utf8' COLLATE 'utf8_bin'");
